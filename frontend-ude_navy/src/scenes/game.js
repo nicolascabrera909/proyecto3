11c8795b0d555529efe546c9275757493569d675
sinsausti <sinsausti.uy@gmail.com>
2022-02-13 00:27:01 -0300

    Update game.js

--- frontend-ude_navy/src/scenes/game.js
+++ frontend-ude_navy/src/scenes/game.js
@@ -16,28 +16,36 @@ class Game extends Phaser.Scene {
   }
 
   showSubmarino(){
-    this.add.image(this.sys.game.config.width -480, this.sys.game.config.height -220, 'submarino').setDisplaySize(50, 10);
+      //this.add.image(this.sys.game.config.width -480, this.sys.game.config.height -220, 'submarino').setDisplaySize(50, 10);
+    var randomX = Phaser.Math.Between(400, this.sys.game.config.width-50);
+    var randomY = Phaser.Math.Between(50, this.sys.game.config.height-50);
+    this.add.image(randomX, randomY, 'submarino').setDisplaySize(50, 10).flipX=true;
   }
   
   showDestructor(){
-    this.add.image(this.sys.game.config.width -480, this.sys.game.config.height -250, 'destructor').setDisplaySize(37, 13).setOrigin(-5, -3);
+    //this.add.image(this.sys.game.config.width -480, this.sys.game.config.height -250, 'destructor').setDisplaySize(37, 13).setOrigin(-5, -3);
+    var randomX = Phaser.Math.Between(50, 300);
+    var randomY = Phaser.Math.Between(50, this.sys.game.config.height-50);
+    this.add.image(randomX, randomY, 'destructor').setDisplaySize(50, 10);
   }
 
   showCargueros(){
+    var randomX = Phaser.Math.Between(50, 200);
+    var randomY = Phaser.Math.Between(50, this.sys.game.config.height-50);
     let x=0;
     let y=0;
     let i=0;
     for (i; i < 3; i++) {
-      this.add.image(this.sys.game.config.width -270, this.sys.game.config.height -230, 'carguero').setDisplaySize(19, 7).setOrigin(-x, -y);  
-      x++;
-      y++;
+      //this.add.image(this.sys.game.config.width -270, this.sys.game.config.height -230, 'carguero').setDisplaySize(19, 7).setOrigin(-x, -y);  
+      this.add.image(randomX, randomY, 'carguero').setDisplaySize(19, 7).setOrigin(x, y);  
+      y=y+2;
     }
-    x=x-3;
-    y--;
+    x=x-2;
+    y=y-2;
     for (i; i > 0; i--) {
-      this.add.image(this.sys.game.config.width -270, this.sys.game.config.height -200, 'carguero').setDisplaySize(19, 7).setOrigin(-x, -y);
-      x++;
-      y--;
+      //this.add.image(this.sys.game.config.width -270, this.sys.game.config.height -200, 'carguero').setDisplaySize(19, 7).setOrigin(-x, -y);
+      this.add.image(randomX, randomY, 'carguero').setDisplaySize(19, 7).setOrigin(x, y);
+      y=y-2;
     }
   }
 
