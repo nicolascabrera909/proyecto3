477bc0ea099097d548383b3d612e099640d97e94
Darkwiner <tomas_cancela18@hotmail.com>
2022-02-16 19:35:47 -0300

    Update submarino.js

--- frontend-ude_navy/src/objects/submarino.js
+++ frontend-ude_navy/src/objects/submarino.js
@@ -3,11 +3,6 @@ import Canion from "./canon.js";
 import Bullets from "./bullets.js";
 
 class Submarino extends Phaser.GameObjects.Sprite {
-  //anterior
-  /*constructor(scene){
-       this.scene = scene;
-    }*/
-
   constructor(scene, x, y, type) {
     super(scene, x, y, type);
     scene.add.existing(this);
@@ -24,10 +19,6 @@ class Submarino extends Phaser.GameObjects.Sprite {
     this.submarino.setDisplaySize(50, 10);
     this.submarino.flipX = true;
     this.spacebar = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
-
-    //-----> ver que hacer con la lineas comentadas
-    //this.submarino.setCollideWorldBounds(true);
-    //this.submarino.setImmovable();
   }
 
   get() {
@@ -39,11 +30,6 @@ class Submarino extends Phaser.GameObjects.Sprite {
     this.submarino.is_destroyed = true;
 
   }
-
-
-  
-
-
 
   shootTorpedo() {
     this.torpedo = new Torpedo(this.scene, this.submarino.x, this.submarino.y, 'torpedo')
@@ -61,8 +47,6 @@ class Submarino extends Phaser.GameObjects.Sprite {
     this.bullet = new Bullets();
     this.bullet.fireBullet(this.x, this.y);
   }
-
-
 
   moveSubmarino() {
     if (!this.submarino.is_destroyed) {
@@ -102,84 +86,7 @@ class Submarino extends Phaser.GameObjects.Sprite {
         }
       }
     }
-
-    //---------------------> Ver que hacer con los comentarios, se puede borrar
-    // /*mover derecha*/
-    // this.scene.input.keyboard.on('keydown-RIGHT', () => {
-    //   //console.log(Phaser.Input.Keyboard.KeyCodes);
-    //   this.submarino.x += 10;
-    // });
-    // /*mover izquierda*/
-    // this.scene.input.keyboard.on('keydown-LEFT', () => {
-    //   //console.log(Phaser.Input.Keyboard.KeyCodes);
-    //   this.submarino.x -= 10; 
-    // });
-    // /*mover arriba*/
-    // this.scene.input.keyboard.on('keydown-UP', () => {
-    //  // console.log(Phaser.Input.Keyboard.KeyCodes);
-    //   this.submarino.y -= 10;
-    // });
-    // /*mover abajo*/
-    // this.scene.input.keyboard.on('keydown-DOWN', () => {
-    //  // console.log(Phaser.Input.Keyboard.KeyCodes);
-    //   this.submarino.y += 10;
-    // });
-    // /*giro izquierda*/
-    // this.scene.input.keyboard.on('keydown-A', () => {
-    //  // console.log(Phaser.Input.Keyboard.KeyCodes);
-    //   this.submarino.flipX = true;
-    // });
-    //  /*giro derecha*/
-    //  this.scene.input.keyboard.on('keydown-S', () => {
-    //   console.log(Phaser.Input.Keyboard.KeyCodes);
-    //   this.submarino.flipX = false;
-    // });
-    //////////////
-
-
-    //this.right = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
-    //this.left =  this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
-    /*
-    this.submarino.setScale(0.5);
-    this.submarino.flipX = false;
-    this.submarino.setOrigin(0.5);  
-    // Fisicas
-    this.submarino.setCollideWorldBounds(true);
-    this.submarino.setBounce(0.3);
-    this.submarino.setAcceleration(50, 0);
-    */
-
-
-    //this.submarino.body.allowGravity = false;
-    //this.submarino = this.physics.add.sprite(randomX, randomY, 'submarino').setDisplaySize(50, 10).flipX=true;
-
-    //this.submarino.setCollideWorldBounds(true);
-    //this.ball.setBounce(1)
-    //this.cursors = this.input.keyboard.createCursorKeys()
   }
-
-  /* ----> esto se hace en create, podriamos borrar esto
-  showSubmarino(){
-    // no se esta usando
-    var randomX = Phaser.Math.Between(400, this.scene.game.config.width-50);
-    var randomY = Phaser.Math.Between(50, this.scene.game.config.height-50);
-    //this.submarino = this.scene.physics.add.image(randomX, randomY, "submarino")
-    this.submarino = this.scene.physics.add.image(randomX, randomY, "submarino")
-    this.submarino.setCollideWorldBounds(true);
-    this.submarino.setDisplaySize(50, 10)
-   // this.submarino.setOrigen(0,0);
-    this.submarino.flipX=true;
-    //this.submarino.setCollideWorldBounds(true);
-    //this.scene.physics.add.collider(this.cuerpo[0], this.cuerpo[i], () => this.choca());
-    //this.cursorKeys=this.scene.input.keyboard.createCursorKeys();
-    this.spacebar=this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
-
-  }
-  */
-
-
-
-
 }
 
 export default Submarino;
