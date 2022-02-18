d4ebb62d8662b1d591116ffa4be7d16e99d2c14d
nicolascabrera909 <nicolascabrera909@hotmail.com>
2022-02-17 23:45:53 -0300

    va

--- backend-ude_navy/services/Games.js
+++ backend-ude_navy/services/Games.js
@@ -18,27 +18,81 @@ class Gamers {
     }
 
     laodGame() {
-         //carga la partida
+        //carga la partida
     }
 
 
     /**Este metodo devuelve un arreglo de x e y*/
-    coordenadasSubmarino(bandoBaco,tamañoXMapa,tamañoYmapa){
+    coordenadasSubmarino(bandoBaco, tamañoXMapa, tamañoYmapa) {
 
         /// cordenadas del mapa inicial es  width: 800,    height: 600,
-        var xTotal=800;
-        var yTotal=600;
-        var mapaMitad=xTotal/2;
+        var xTotal = 800;
+        var yTotal = 600;
+        var mapaMitad = xTotal / 2;
 
         //calculo numero ramdom
-        var x1 = Math.random()*(mapaMitad - xTotal)+mapaMitad;
-        var y1 = Math.random()*(yTotal - 0)+0;
+        var x1 = Math.random() * (mapaMitad - xTotal) + mapaMitad;
+        var y1 = Math.random() * (yTotal - 0) + 0;
         coordenada = {
-            "x":x1,
-            "y":y1
+            "x": x1,
+            "y": y1
         };
-        return  coordenada;
+        return coordenada;
     }
+
+    /**Este metodo devuelve un arreglo de x e y*/
+    coordenadasCargueros(bandoBaco, tamañoXMapa, tamañoYmapa) {
+
+        /// cordenadas del mapa inicial es  width: 800,    height: 600,
+        var xTotal = 800;
+        var yTotal = 600;
+        var mapaMitad = xTotal / 2;
+        var listaCoordenadas = [];
+
+
+
+        coordenada = {
+            "x": Math.random() * (mapaMitad - 0) + 0,
+            "y": Math.random() * (yTotal - 0) + 0
+        };
+        listaCoordenadas.pushcoordenada
+        for (var i = 0; i < 5; i++) {
+            if (i < 3) {
+                coordenada.y = y + 10;
+            } else {
+                if (i == 3) {
+                    coordenada.x = x + 10;
+                }
+                coordenada.y = y + 10;
+            }
+        }
+        var j = 0;
+        coordenada = {
+            "x": Math.random() * (mapaMitad - 0) + 0,
+            "y": Math.random() * (yTotal - 0) + 0
+        };
+        var noEsIgual = false;
+        var contador = 0;
+        while (!esIgual) {
+            for (var j = 0; j < listaCoordenadas.length; j++) {
+                if (coordenada.x == listaCoordenadas[j].x && coordenada.y == listaCoordenadas[j].y) {
+                    coordenada = {
+                        "x": Math.random() * (mapaMitad - 0) + 0,
+                        "y": Math.random() * (yTotal - 0) + 0
+                    };
+                } else {
+                    noEsIgual = true;
+
+                }
+            }
+            j++;
+        }
+
+
+        return listaCoordenadas;
+    }
+
+
 
 
 }
