7cd321f1eef8893f8b58ef5feced0ec161e576d7
sinsausti <sinsausti.uy@gmail.com>
2022-02-21 23:43:16 -0300

    Frontend new2

--- frontend-ude_navy-new/src/main.js
+++ frontend-ude_navy-new/src/main.js
@@ -1,10 +1,5 @@
 //IMPORTS
-import Bootloader from "./bootloader.js";
-// import Menu from "./src/scenes/menu.js";
-// import Credits from "./src/scenes/credits.js";
 import Game from "./scenes/game.js";
-// import Help from "./src/scenes/help.js";
-// import Choose from "./scenes/choose.js";
 
 // CONFIG BASICA
 const config = {
@@ -13,7 +8,6 @@ const config = {
     height: 600,
     type: Phaser.AUTO,
     parent: 'container',
-    //backgroundColor: '#f9ca24',
     pixelArt: true,
     physics: {
         default: "arcade",
@@ -22,7 +16,7 @@ const config = {
           }
     },
     title: '1.0',
-    scene: [Bootloader, Game],
+    scene: [Game],
 }
 
 new Phaser.Game(config);
