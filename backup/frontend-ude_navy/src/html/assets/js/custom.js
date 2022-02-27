bbe80621d3b9e119c815aa6c70cc347f7f8ffb0f
sinsausti <sinsausti.uy@gmail.com>
2022-02-27 16:29:29 -0300

    se ordena repo

--- backup/frontend-ude_navy/src/html/assets/js/custom.js
+++
@@ -1,8 +0,0 @@
-$(".choice").on("click", function(){
-    console.log("test");
-    $(".choice").removeClass("expand unset ");
-    $(".choice").addClass("small");
-    $(this).removeClass("small");
-    $(this).addClass("expand");
-}
-)
