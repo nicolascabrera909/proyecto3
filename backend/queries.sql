0449f415c75d24ed90abcea030a3b33d6740ddff
sinsausti <sinsausti.uy@gmail.com>
2022-03-03 23:27:19 -0300

    Update queries.sql

--- backend/queries.sql
+++ backend/queries.sql
@@ -1,10 +1,7 @@
 CREATE DATABASE udenavybd;
-GRANT ALL PRIVILEGES ON udenavybd.* TO udeuser IDENTIFIED BY 'udepass';
-
+CREATE USER 'udeuser'@'%' IDENTIFIED BY 'udepass';
+GRANT ALL PRIVILEGES ON udenavybd.* TO udeuser@'%';
 USE udenavybd;
-SET PASSWORD FOR 'udeuser'@'%' = PASSWORD('udepass');
-
--- TABLE Version
 CREATE TABLE version (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   version VARCHAR(10) NOT NULL,
@@ -12,10 +9,3 @@ CREATE TABLE version (
 );
 INSERT INTO version (id, version)
   VALUES (1, '0.1');
--- SELECTS
-SELECT * FROM udenavybd.version;
-DELETE FROM udenavybd.version;
--- DROPS
-DROP table udenavybd.version;
--- DROP DB
-DROP database udenavybd;
