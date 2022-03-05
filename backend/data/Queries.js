3420b0ec6594da7f6872884566f7c150277017eb
nicolascabrera909 <nicolascabrera909@hotmail.com>
2022-03-05 14:28:52 -0300

    va

--- backend/data/Queries.js
+++ backend/data/Queries.js
@@ -17,7 +17,7 @@ class Queries {
         const query='insert into difficulty (id,multiMap,multiLife,multiPower,multiVisibility,multiSpeed,multiDistance ) values(?,?,?,?,?,?,?)';
         return query;
     }
-    obtengoDificultad(){
+    findDificultad(){
         const query='SELECT id,multiMap,multiLife,multiPower,multiVisibility,multiSpeed,multiDistance from difficulty  where id=?';
         return query;
     }
@@ -26,10 +26,44 @@ class Queries {
         const query='insert into game (id,difficulty_id ) values(?,?)';
         return query;
     }
-    obtengoGame(){
+    findGame(){
         const query='SELECT id,difficulty_id from game where id=?';
         return query;
     }
+
+    insertMap(){
+        const query='insert into mapa (id,game_id,heigth,width ) values(?,?,?,?);';
+        return query;
+    }
+    findMap(){
+        const query='SELECT id,game_id,heigth,width from mapa where game_id=?';
+        return query;
+    }
+    insertMap(){
+        const query='insert into mapa (id,game_id,heigth,width ) values(?,?,?,?);';
+        return query;
+    }
+    findMap(){
+        const query='SELECT id,heigth,width from mapa where game_id=?';
+        return query;
+    }
+    insertPlayer(){
+        const query='insert into player (id,name,game_id) values(?,?,?);';
+        return query;
+    }
+    findPlayer(){
+        const query='select id,name from player where game_id =?';
+        return query;
+    }
+    insertPlayer(){
+        const query='insert into player (id,name,game_id) values(?,?,?);';
+        return query;
+    }
+    findPlayer(){
+        const query='select id,name from player where game_id =?';
+        return query;
+    }
+
 
 }
 
