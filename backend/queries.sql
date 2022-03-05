6923e510278fc69cda4a19e81621cd873cb205ef
sinsausti <sinsausti.uy@gmail.com>
2022-03-04 21:08:05 -0300

    Update queries.sql

--- backend/queries.sql
+++ backend/queries.sql
@@ -9,3 +9,111 @@ CREATE TABLE version (
 );
 INSERT INTO version (id, version)
   VALUES (1, '0.1');
+
+
+
+
+use udenavybd;
+
+CREATE TABLE game (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	difficulty_id INT,
+	FOREIGN KEY (difficulty_id) REFERENCES difficulty(id),
+	created_at timestamp NOT NULL DEFAULT current_timestamp
+);
+
+CREATE TABLE mapa (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	game_id INT,
+	heigth INT,
+	width INT,
+	FOREIGN KEY (game_id) REFERENCES game(id),
+	created_at timestamp NOT NULL DEFAULT current_timestamp
+);
+
+CREATE TABLE difficulty (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	multiMap INT,
+	multiLife INT,
+	multiPower INT,
+	multiVisibility INT,
+	multiSpeed INT,
+	multiDistance INT,
+	created_at timestamp NOT NULL DEFAULT current_timestamp
+);
+
+CREATE TABLE player (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(20),
+	game_id INT,
+	FOREIGN KEY (game_id) REFERENCES game(id),
+	created_at timestamp NOT NULL DEFAULT current_timestamp
+);
+
+CREATE TABLE freighters (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	player_id INT,
+	positionX INT,
+	positionY INT,
+	boatLife INT,
+	boatType INT,
+	visibility INT,
+	FOREIGN KEY (player_id) REFERENCES player(id),
+	created_at timestamp NOT NULL DEFAULT current_timestamp
+);
+
+CREATE TABLE ship (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	player_id INT,
+	positionX INT,
+	positionY INT,
+	boatLife INT,
+	boatType INT,
+	visibility INT,
+	FOREIGN KEY (player_id) REFERENCES player(id),
+	created_at timestamp NOT NULL DEFAULT current_timestamp
+);
+
+CREATE TABLE destructor (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	ship_id INT,
+	FOREIGN KEY (ship_id) REFERENCES ship(id),
+	created_at timestamp NOT NULL DEFAULT current_timestamp
+);
+
+CREATE TABLE submarine (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	ship_id INT,
+	s_depth INT,
+	FOREIGN KEY (ship_id) REFERENCES ship(id),
+	created_at timestamp NOT NULL DEFAULT current_timestamp
+);
+
+CREATE TABLE cannon (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	ship_id INT,
+	c_power INT,
+	c_distance INT,
+	FOREIGN KEY (ship_id) REFERENCES ship(id),
+	created_at timestamp NOT NULL DEFAULT current_timestamp
+);
+
+CREATE TABLE depth_charge (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	destructor_id INT,
+	dp_time INT,
+	dp_depth INT,
+	dp_power INT,
+	dp_distance INT,
+	FOREIGN KEY (destructor_id) REFERENCES destructor(id),
+	created_at timestamp NOT NULL DEFAULT current_timestamp
+);
+
+CREATE TABLE torpedo (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	submarine_id INT,
+	t_power INT,
+	t_distance INT,
+	FOREIGN KEY (submarine_id) REFERENCES submarine(id),
+	created_at timestamp NOT NULL DEFAULT current_timestamp
+);
